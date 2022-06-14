11abf20f2a1180a440f21b2fb3fae519232e6019
Izma Raifani <izurai@gmail.com>
2022-06-14 15:45:39 +0700

    add component:button styled

---
+++ src/StyledComponents/Button.styled.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+const ButtonStyled = styled.button`
+  display: flex;
+  padding:10px; 
+  text-align : center ;
+  outline:none;
+  border:none;
+  justify-content: center;
+  background-color: hsl(var(--clr-dark)) ;
+  color: hsl(var(--clr-white)) ;
+  cursor: pointer;
+`
+export default ButtonStyled
