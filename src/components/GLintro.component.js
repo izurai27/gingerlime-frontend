5beb678c8962ed2521bda271bcf6673c244002cc
Izma Raifani <izurai@gmail.com>
2022-06-14 19:26:17 +0700

    mobile design2

--- src/components/GLintro.component.js
+++ src/components/GLintro.component.js
@@ -5,7 +5,7 @@ import RoundContainerStyled from '../StyledComponents/RoundContainer.styled'
 
 const GLintro = () => {
   return (
-    <ContainerStyled width="90%"  display="grid" gap="20px" padding="20px" border="1px solid  hsl(var(--clr-light))">
+    <ContainerStyled width="95%"  display="grid" gap="20px" padding="20px" border="1px solid  hsl(var(--clr-light))">
       <ContainerStyled width="fit-content" display="flex" gap="20px">
         <RoundContainerStyled width="20vw" maxwidth="100px" >
           <img style={{"width":"100%", "height":"100%"}} src="https://scontent.fcgk11-1.fna.fbcdn.net/v/t1.6435-9/109939387_2609982575982004_2991628643924282322_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=cdbe9c&_nc_eui2=AeH19ZPrPVfbsS-g6O-YkfFazcbcbaI7hurNxtxtojuG6pGLu-sARIiiJh0EPxqCTlM&_nc_ohc=hRjd0iMQFgEAX_xkd2H&_nc_ht=scontent.fcgk11-1.fna&oh=00_AT_ObDJFvG8nq3KtrPZ956vSbB6ULcysZeyxdcSRQXY8sw&oe=62CC0489" alt="" />
